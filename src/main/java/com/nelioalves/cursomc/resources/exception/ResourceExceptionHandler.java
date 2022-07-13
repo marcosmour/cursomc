de5151038844430d1daf5701f793b58b011a6bf0
Marcos Moura <marcosmoura201@hotmail.com>
2022-07-13 12:14:13 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,4 +30,16 @@ public class ResourceExceptionHandler {
 		StandardErro err = new StandardErro(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class) // ESTA LIGADA DIRETAMENTE COM O DELETE DE CATEGORIAS. AULA 36. 09:20
+	public ResponseEntity<StandardErro> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+	
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
 }
+
